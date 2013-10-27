4c018b036c6594c9d77dcf9d93037b048700c444
merchleg <mtglum@gmail.com>
2013-10-27 13:58:43 -0700

    Added Basic functionality to Account Management Screen

--- Social Solver/Social Solver/ViewControllers/AccountManagementViewController.m
+++ Social Solver/Social Solver/ViewControllers/AccountManagementViewController.m
@@ -68,12 +68,47 @@
     
 }
 
+-(BOOL)checkNameUnique:(NSString*)name
+{
+    NSManagedObjectContext *mc = [(AppDelegate*)[[UIApplication sharedApplication] delegate] managedObjectContext];
+    NSEntityDescription* entityDescription = [NSEntityDescription entityForName:@"User"
+                                                         inManagedObjectContext:mc];
+    NSFetchRequest* req = [[NSFetchRequest alloc] init];
+    [req setEntity:entityDescription];
+    
+    NSPredicate* namePredicate = [NSPredicate predicateWithFormat:@"%K like %@", @"name", name];
+    
+    [req setPredicate:namePredicate];
+    
+    NSArray* users = [mc executeFetchRequest:req error:nil];
+    
+    for (NSManagedObject* us in users) {
+        if (![_editingUser isEqual:us]) {
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
+
+
 -(IBAction)save:(id)sender
 {
     NSManagedObjectContext *mc = [(AppDelegate*)[[UIApplication sharedApplication] delegate] managedObjectContext];
     NSEntityDescription* entityDescription = [NSEntityDescription entityForName:_userType
                                                          inManagedObjectContext:mc];
     NSManagedObject* user = [[NSManagedObject alloc] initWithEntity:entityDescription insertIntoManagedObjectContext:mc];
+    
+    if (![self checkNameUnique:[_nameField text]]) {
+        [[[UIAlertView alloc] initWithTitle:@"Cannot Save" message:@"A user already has the same name." delegate:nil cancelButtonTitle:@"OKay" otherButtonTitles:nil] show];
+        return;
+    }
+    
+    if (![[_passwordField text] isEqualToString:[_passwordConfirmationField text]]) {
+        [[[UIAlertView alloc] initWithTitle:@"Cannot Save" message:@"Please make sure that you enter the same password in each field" delegate:nil cancelButtonTitle:@"OKay" otherButtonTitles:nil] show];
+        return;
+    }
     
     [user setValue:[_nameField text] forKey:@"name"];
     [user setValue:[_passwordField text] forKey:@"passhash"];
